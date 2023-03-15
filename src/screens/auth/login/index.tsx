b8d75173ffe0204d8d0cd3a1f15550ca1556a6c3
SaimNasser <111045215+saimnasser1@users.noreply.github.com>
2023-03-15 19:38:11 +0500

    enabled hermes on ios & android

--- src/screens/auth/login/index.tsx
+++ src/screens/auth/login/index.tsx
@@ -1,14 +1,20 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import { ScreenWrapper } from 'react-native-screen-wrapper';
 import { Input } from '~components';
 import styles from './styles';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { LoginFormValidation } from './utils';
 import { Theme } from '~utils';
+
 const Login = ({ navigation }: NativeStackScreenProps<any>) => {
+  if (typeof HermesInternal === 'undefined') {
+    console.log('Hermes is not enabled');
+  } else {
+    console.log('Hermes is enabled');
+  }
   const {
     control,
     formState: { isValid },
@@ -20,15 +26,7 @@ const Login = ({ navigation }: NativeStackScreenProps<any>) => {
     <ScreenWrapper statusBarColor={Theme.colors.primary} scrollType="keyboard">
       <View style={styles.container}>
         <View>
-          <Input control={control} name="email" label={'Email'} />
-          <Input control={control} name="password" label={'Password'} />
         </View>
-        {/* <Button
-          onPress={() => navigation.navigate(ScreenNames.FORGOT_PASSWORD)}
-          mode="contained"
-          disabled={!isValid}>
-          Login
-        </Button> */}
       </View>
     </ScreenWrapper>
   );
